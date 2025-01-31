20a95a118ca92a71b6809189d4e9e2b68d8c5a8d
raviteja129 <30767189+raviteja129@users.noreply.github.com>
2025-01-30 19:23:33 -0500

    Update index.js
    udpate

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const urlencodedParser = bodyParser.urlencoded({extended:false})
 const saltRounds = 10
 
 const app = express()
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: false}))
 app.use(expressLayouts)
